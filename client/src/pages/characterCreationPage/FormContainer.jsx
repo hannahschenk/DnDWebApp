27cfe7d887033bac5556d07bec099f23dbbe92f6
Hunter Long <hunterlong@admins-MacBook-Pro.local>
2021-01-21 14:22:15 -0600

    AVDSFR-32 Made the details component added it to the Form Container. should display on every form page

--- client/src/pages/characterCreationPage/FormContainer.jsx
+++ client/src/pages/characterCreationPage/FormContainer.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import constants from "./../../utils/constants";
 import CharacterCreationForm from "./../../components/characterCreationForm"
+import Details from "../../components/characterCreationForm/Details"
 const FormContainer = ({sectionIdxState}) => {
     /*NOTE: might change later if state becomes global*/
     const {currentSectionIdx, currentSectionIdxReducer} = sectionIdxState;
@@ -17,6 +18,7 @@ const FormContainer = ({sectionIdxState}) => {
                 <h3>Details</h3>
                 <section>
                     {/*populate with details later*/}
+                    <Details />
                 </section>
             </article>
         </section>
