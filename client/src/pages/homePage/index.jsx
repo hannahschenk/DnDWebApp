c75dd2a4c10bcd97f5c9ddeee269b6902b090c33
hschenkTechtonic <hannah.schenk@techtonic.com>
2021-01-29 17:28:56 -0600

    feat: ticket 43, home and sign in sections complete

--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -7,36 +7,45 @@ const homePage = () => {
         <>
         <div className = "landingContainer">
             <section className = "character">
-                <div className = "character__container">
-                    <h2 className = "container__header">Anonymous Character Creation</h2>
-                    <p className = "container__text">Creating a character has never been easier with the help of DndCC. This character creation has been
-                        simplified down to a step by step process where we walk you through. This sheet will take you through the Race, Classes,
-                        Ability scores, Character details, Proficiencies and Equipment. By creating your character anonymously, you will not be
-                        able to save your character sheet for later use, but you can still use it immediately after creation.
-                    </p>
-                </div> 
-                <i class='fab fa-d-and-d'></i>
-                <Link to="/create-character" className = "character__button button button--red">Create Character</Link>
+                <div className = "character__container1">
+                    <div className = "container1__container">
+                        <h2 className = "container__header">Anonymous Character Creation</h2>
+                        <p className = "container__text">Creating a character has never been easier with the help of DndCC. This character creation has been
+                            simplified down to a step by step process where we walk you through. <br></br><br></br>This sheet will take you through the Race, Classes,
+                            Ability scores, Character details, Proficiencies and Equipment.<br></br><br></br> By creating your character anonymously, you will not be
+                            able to save your character sheet for later use, but you can still use it immediately after creation.
+                        </p>
+                    </div>
+                    <i className='fab fa-d-and-d'></i>
+                    <div className = "container1__buttonContainer">
+                        <Link to="/create-character" className = "button button button--red">CREATE CHARACTER</Link>
+                    </div>
+                </div>
             </section>
             <section className = "login">
                 <div className = "login__container">
-                    <i class="fas fa-dice-d20"></i>
-                    <div className = "container__signin">
-                        <div className = "signin__google">
-                            <img src = "" className = "google__image"></img>
-                            <a className = "google__text" href="#">Sign in with google</a>
+                    <div className = "filler1"></div>
+                    <div className = "container__container1">
+                        <div className = "container1__signin">
+                        <i className="fas fa-dice-d20"></i>
+                            <div className = "signin__google">
+                                <img src = "" className = "google__image"></img>
+                                <a className = "google__text" href="#">Sign in with google</a>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className = "login__container2">
-                    <div className = "container2__textContainer">
-                        <h2 className = "textContainer__header">Authenticated Character Creation</h2>
-                        <p className = "textContainer__text">Creating a character under an authenticated account, will not only allow you to save your created character,
-                            but to also save multiple created characters. These can be accessed at any time, and edited upon later.
-                        </p>
-                        <br></br>
+                    <div className = "container__container2">
+                        <div className = "container2__textContainer">
+                            <h2 className = "textContainer__header">Authenticated Character Creation</h2>
+                            <p className = "textContainer__text">Creating a character under an authenticated account, will not only allow you to save your created character,
+                                but to also save multiple created characters. These can be accessed at any time, and edited upon later.
+                            </p>
+                            <br></br>
+                        </div>
                     </div>
-                </div> 
+                    <div className = "filler2"></div> 
+                </div>
+                
             </section>
             <section className = "about">
                 <div className = "about__container">
