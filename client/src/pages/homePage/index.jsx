89ffac5e6dbbdfc18e71e36ce2360b0a9842f9db
hschenkTechtonic <hannah.schenk@techtonic.com>
2021-01-31 22:54:42 -0600

    feat: ticket 43, completed home page styling

--- client/src/pages/homePage/index.jsx
+++ client/src/pages/homePage/index.jsx
@@ -10,9 +10,9 @@ const homePage = () => {
                 <div className = "character__container1">
                     <div className = "container1__container">
                         <h2 className = "container__header">Anonymous Character Creation</h2>
-                        <p className = "container__text">Creating a character has never been easier with the help of DndCC. This character creation has been
-                            simplified down to a step by step process where we walk you through. <br></br><br></br>This sheet will take you through the Race, Classes,
-                            Ability scores, Character details, Proficiencies and Equipment.<br></br><br></br> By creating your character anonymously, you will not be
+                        <p className = "container__text">Creating a character has never been easier with the help of D&D CC. This character creation tool has been
+                            simplified to a process where we walk you through each step. <br></br><br></br>This sheet will take you through the race, classes,
+                            ability scores, character details, proficiencies, and equipment for the character you want tp create.<br></br><br></br> By creating your character anonymously, you will not be
                             able to save your character sheet for later use, but you can still use it immediately after creation.
                         </p>
                     </div>
@@ -26,19 +26,22 @@ const homePage = () => {
                 <div className = "login__container">
                     <div className = "filler1"></div>
                     <div className = "container__container1">
-                        <div className = "container1__signin">
                         <i className="fas fa-dice-d20"></i>
+                        <div className = "container1__signin">
                             <div className = "signin__google">
-                                <img src = "" className = "google__image"></img>
-                                <a className = "google__text" href="#">Sign in with google</a>
+                                <div className = "google__image">
+                                    <i className="fab fa-google"></i>
+                                </div>
+                                <div className = "gridColor"></div>
+                                <a className = "google__text" href="#">Sign in with Google</a>
                             </div>
                         </div>
                     </div>
                     <div className = "container__container2">
                         <div className = "container2__textContainer">
                             <h2 className = "textContainer__header">Authenticated Character Creation</h2>
-                            <p className = "textContainer__text">Creating a character under an authenticated account, will not only allow you to save your created character,
-                                but to also save multiple created characters. These can be accessed at any time, and edited upon later.
+                            <p className = "textContainer__text">Creating a character under an authenticated account will not only allow you to save your created character,
+                                but to also save multiple character sheets. These can be accessed at any time, and edited later.
                             </p>
                             <br></br>
                         </div>
@@ -50,13 +53,15 @@ const homePage = () => {
             <section className = "about">
                 <div className = "about__container">
                     <h2 className = "container__header">About us</h2>
-                    <p className = "container__text">We're a team of apprentices, The School Of Five Rats, and are apart of the Techtonic team. We are focused on developing 
-                        our skills as software developers by creating this app. Our group members include, Ervey Del Rosario, Hunter Long, Ian Goodwin, 
-                        Hannah Schenk and Tre Silknitter. 
+                    <p className = "container__text">We're a team of apprentices, The School Of Five Rats, and are a part of the Techtonic apprenticeship program. We are focused on developing 
+                        our skills as software developers by creating this app. Our group members include Ervey Del Rosario, Hunter Long, Ian Goodwin, 
+                        Hannah Schenk, and Tre Silknitter. 
                     </p>
                 </div>
-                <img src = "" className = "about__image1"></img>
-                <img src = "" className = "about__image2"></img>
+                <div className = "about__imageContainer">
+                    <i class="fas fa-dice"></i>
+                    <i class="fas fa-dice-four"></i>
+                </div>
             </section>
         </div>
         </>
