b072b82cd8a49149e390ba49a9370f1f8a06719e
edelrosarioTechtonic <ervey.delrosario@techtonic.com>
2021-02-02 14:56:50 -0800

    ldjsfhuishdfjal;

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,9 +2,9 @@ const router = require('express').Router();
 const apiRoutes = require("./api");
 //const jwtCheck = require("./../config/jwtConfig");
 
-router.use("/api", apiRoutes)
+//router.use("/api", apiRoutes)
 
-router.get('/*', (req, res) => {
+router.use(function (req, res) {
   res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
 });
 module.exports = router; 
