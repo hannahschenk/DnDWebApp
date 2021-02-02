2d0682a7c78b9217d5d6fc16d8cd7c77b9bbd991
edelrosarioTechtonic <ervey.delrosario@techtonic.com>
2021-02-02 14:11:53 -0800

    commented out all auth0

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require("./api");
 const jwtCheck = require("./../config/jwtConfig");
 
-router.use("/api", jwtCheck, apiRoutes)
+//router.use("/api", jwtCheck, apiRoutes)
 router.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '..', 'client', 'dist', 'index.html'));
   });
